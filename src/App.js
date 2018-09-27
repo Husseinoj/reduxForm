f0752f8e3088cf75934d05d6f63d15794c09933e
Hussein <Hussein@gmail.com>
2018-09-27 19:17:24 +0330

    added form validation

--- src/App.js
+++ src/App.js
@@ -88,6 +88,44 @@ let SignInForm = props => {
   </form>;
 };
 
+
+const validate = val => {
+  const errors = {};
+  if (!val.firstName) {
+    console.log('First Name is required');
+    errors.firstName = 'Required';
+  }
+  if (!val.lastName) {
+    console.log('Last Name is required');
+    errors.lastName = 'Required';
+  }
+  if (!val.email) {
+    console.log('email is required');
+    errors.email = 'Required';
+  } else if (!/^.+@.+$/i.test(val.email)) {
+    console.log('email is invalid');
+    errors.email = 'Invalid email address';
+  }
+  if (!val.age) {
+    errors.age = 'Required'
+  } else if (isNaN(Number(val.age))) {
+    errors.age = 'Must be a number'
+  } else if (Number(val.age) < 18) {
+    errors.age = 'Sorry, you must be at least 18 years old'
+  }
+  return errors;
+};
+
+const renderField = ({ input, label, type, meta: { touched, error, warning } }) => (
+  <div>
+    <div className="control">
+      <label className="field">{label}</label>
+      <input className="input" {...input} placeholder={label} type={type}/>
+      {touched && ((error && <span>{error}</span>) || (warning && <span>{warning}</span>))}
+    </div>
+  </div>
+)
+
 class App extends Component {
   render() {
     return (
