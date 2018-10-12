23c7d5eec24b69978af03b3b0b9d18e6fa837978
Hussein <Hussein@gmail.com>
2018-10-12 16:58:17 +0330

    implement wizard form

--- src/App.js
+++ src/App.js
@@ -1,28 +1,19 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
 import './App.css';
-import ContactForm from './ContactForm';
-
+import WizardForm from './WizardForm';
 class App extends Component {
-  handle=(values)=>{
-    console.log("parentSubmit",values);
-  }
-
+ 
+  submitHandle = values =>{
+    console.log("Submit Form",values);
+  };
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React x redux-form</h1>
-        </header>
-        <div className="container">
-          <p className="App-intro">
-            Contact Form
-          </p>
-        <ContactForm onSubmit={this.handle}/>
-        </div>
+      <div>
+        <h2>Hello Man</h2>
+        <WizardForm onSubmit={this.submitHandle}/>
       </div>
-    );
+    )
   }
 }
 
